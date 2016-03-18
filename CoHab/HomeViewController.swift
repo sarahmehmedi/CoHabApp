cc0731762a32c9da6094ad249918fe43236bcb72
Nieky Allen <dallen4@luc.edu>
2016-03-18 13:51:29 -0500

    HomeViewController
    I started the basic wire frame for the Home screen and started setting
    up the basic onClick functions.

---
+++ CoHab/HomeViewController.swift
@@ -0,0 +1,66 @@
+
+//
+//  HomeViewController.swift
+//  CoHab
+//
+//  Created by Dominique Allen on 3/18/16.
+//  Copyright © 2016 Christian . All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // This is just a funciton that displays an alert message for when you mess something up
+    func displayMyAlertMessage(userMessage:String)
+    {
+        let myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle:UIAlertControllerStyle.Alert);
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:nil);
+        
+        myAlert.addAction(okAction);
+        
+        self.presentViewController(myAlert, animated: true, completion: nil);
+    }
+    
+    @IBAction func onMyScheduleClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onTasksClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onBillsClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onCalendarClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onGroupClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onBuildingReportsClick(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onSettingsClick(sender: AnyObject) {
+        
+    }
+    
+}
