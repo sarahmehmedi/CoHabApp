dfaa3735295d4b1e9b361dd51843c1620bbe2166
Nieky Allen <dallen4@luc.edu>
2016-03-19 14:52:41 -0500

    Been reading about EventKit, tried importing so I can try it out later on the Calendar module. Created CalendarViewController.swift.

---
+++ CoHab/CalendarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarViewController.swift
+//  CoHab
+//
+//  Created by Dominique Allen on 3/19/16.
+//  Copyright © 2016 Christian . All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class CalendarViewController: UIViewController {
+    
+}
