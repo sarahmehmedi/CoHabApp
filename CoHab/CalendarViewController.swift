02983dce3185b39ad9c3cc516529aad20e18b090
Nieky Allen <dallen4@luc.edu>
2016-04-01 18:19:57 -0500

    started playing with TTEventKit cocoa pod

--- CoHab/CalendarViewController.swift
+++ CoHab/CalendarViewController.swift
@@ -10,8 +10,72 @@ import UIKit
 import EventKit
 import TTEventKit
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, CalendarDelegate {
     
+    @IBOutlet weak var calendarView: CalendarView!
     
+    @IBOutlet weak var header: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        calendarView.delegate = self
+        
+        calendarView.current.year = 2016
+        calendarView.current.month = 3
+        
+        let year = calendarView.current.year
+        let month = calendarView.current.month
+        changedMonth(year, month: month)
+        
+        EventStore.requestAccess() { (granted, error) in
+            if !granted {
+                dispatch_async(dispatch_get_main_queue()) {
+                    let alert = UIAlertController(title: "Error", message: "CoHab does not have access to your calendars.", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.loadView()
+                }
+            }
+        }
+        
+        let ev = EventStore.getEvents(Month(year: 2017, month: 1))
+        
+        if ev  != nil {
+            for e in ev {
+                print("Title \(e.title)")
+                print("startDate: \(e.startDate)")
+                print("endDate: \(e.endDate)")
+            }
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func tappedAddButton(sender: UIBarButtonItem) {
+        
+        let event = EventStore.create()
+        event.title = "Test Event"
+        event.startDate = Month(year: 2016, month: 3).nsdate
+        event.endDate = event.startDate
+        event.notes = "This is a test event"
+        EventUI.showEditView(event)
+    }
+    
+    //=================================
+    // Calendar Delegate
+    //=================================
+    
+    func changedMonth(year: Int, month: Int) {
+        let monthEn = ["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        
+        header.title = "\(year) \(monthEn[month - 1])"
+        
+    }
+    
+    func selectedDay(dayView: CalendarDayView) {
+        
+    }
     
 }
