d4fb37c6a0a70ff38772be619ff2b619de08f1db
sarahmehmedi <sarahmehmedi@gmail.com>
2016-04-05 18:27:23 -0500

    Added billsviewcontroller

---
+++ BillsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BillsViewController.swift
+//  
+//
+//  Created by Sarah Mehmedi on 4/3/16.
+//
+//
+
+import Foundation
---
+++ CoHab/BillsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BillsViewController.swift
+//  
+//
+//  Created by Sarah Mehmedi on 4/3/16.
+//
+//
+
+import UIKit
+
+class BillsViewController:UIViewController{
+    
+    @IBOutlet
+    var tableView: UITableView!
+}
