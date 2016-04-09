f5ac4dab8c7637aa084aee450bde6fdfebb49870
sarahmehmedi <sarahmehmedi@gmail.com>
2016-04-08 19:15:28 -0500

    tried messing around with mgswipetablecell pod for billsview

--- CoHab/BillsViewController.swift
+++ CoHab/BillsViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import MGSwipeTableCell
+
 
 class BillsViewController:UIViewController{
     
-    @IBOutlet
-    var tableView: UITableView!
+    @IBOutlet weak var table: UITableView!
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let reuseIdentifier = "programmaticCell"
+        var cell = self.table.dequeueReusableCellWithIdentifier(reuseIdentifier) as! MGSwipeTableCell!
+        if cell == nil
+        {
+            cell = MGSwipeTableCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+        }
+        
+        cell.textLabel!.text = "Title"
+        cell.detailTextLabel!.text = "Detail text"
+       // cell.delegate = self //optional
+        
+        //configure left buttons
+        cell.leftButtons = [MGSwipeButton(title: "", icon: UIImage(named:"check.png"), backgroundColor: UIColor.greenColor())
+            ,MGSwipeButton(title: "", icon: UIImage(named:"fav.png"), backgroundColor: UIColor.blueColor())]
+        cell.leftSwipeSettings.transition = MGSwipeTransition.Rotate3D
+        
+        //configure right buttons
+        cell.rightButtons = [MGSwipeButton(title: "Delete", backgroundColor: UIColor.redColor())
+            ,MGSwipeButton(title: "More",backgroundColor: UIColor.lightGrayColor())]
+        cell.rightSwipeSettings.transition = MGSwipeTransition.Rotate3D
+        
+        return cell
+    }
+
 }
