c95b32571db039dd3563ccc050c53c4347354678
sarahmehmedi <sarahmehmedi@gmail.com>
2016-04-12 20:15:39 -0500

    Added sliding functionality

--- BillsViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BillsViewController.swift
-//  
-//
-//  Created by Sarah Mehmedi on 4/3/16.
-//
-//
-
-import Foundation
--- CoHab/BillsViewController.swift
+++ CoHab/BillsViewController.swift
@@ -59,18 +59,18 @@ class BillsViewController:UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     // This actually creates our table
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CustomCell
-        cell.billName.text = bills[indexPath.row]
-        cell.billTotal.text = total[indexPath.row]
-        cell.billDueDate.text = dueDate[indexPath.row]
-        return cell
-    }
-    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CustomCell
+//        cell.billName.text = bills[indexPath.row]
+//        cell.billTotal.text = total[indexPath.row]
+//        cell.billDueDate.text = dueDate[indexPath.row]
+//        return cell
+//    }
+//    
 
     
-   
-    /*func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+   //Create table edited so theres a slide left/right function - will have to connect the slide functions to something later
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let reuseIdentifier = "programmaticCell"
         var cell = self.table.dequeueReusableCellWithIdentifier(reuseIdentifier) as! MGSwipeTableCell!
@@ -79,21 +79,22 @@ class BillsViewController:UIViewController, UITableViewDataSource, UITableViewDe
             cell = MGSwipeTableCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
         }
         
-        cell.textLabel!.text = "Title"
-        cell.detailTextLabel!.text = "Detail text"
+        cell.textLabel!.text = bills[indexPath.row]
+        cell.detailTextLabel!.text = total[indexPath.row]
+        cell.detailTextLabel!.text = dueDate[indexPath.row]
         // cell.delegate = self //optional
         
-        //configure left buttons
-        cell.leftButtons = [MGSwipeButton(title: "", icon: UIImage(named:"check.png"), backgroundColor: UIColor.greenColor())
-            ,MGSwipeButton(title: "", icon: UIImage(named:"fav.png"), backgroundColor: UIColor.blueColor())]
+        //configures left buttons
+        cell.leftButtons = [MGSwipeButton(title: "Paid", icon: UIImage(named:"check.png"), backgroundColor: UIColor.greenColor())
+            ,MGSwipeButton(title: "Will Pay", icon: UIImage(named:"fav.png"), backgroundColor: UIColor.orangeColor())]
         cell.leftSwipeSettings.transition = MGSwipeTransition.Rotate3D
         
-        //configure right buttons
+        //configures right buttons
         cell.rightButtons = [MGSwipeButton(title: "Delete", backgroundColor: UIColor.redColor())
             ,MGSwipeButton(title: "More",backgroundColor: UIColor.lightGrayColor())]
         cell.rightSwipeSettings.transition = MGSwipeTransition.Rotate3D
         
         return cell
-    }*/
+    }
 }
 
