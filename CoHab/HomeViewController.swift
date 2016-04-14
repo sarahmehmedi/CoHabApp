ab54fb221d671cfdc18c607217e99579c8cd8557
Nieky Allen <dallen4@luc.edu>
2016-04-14 18:08:09 -0500

    changed "my schedule" to "Calendar" in dropdown

--- CoHab/HomeViewController.swift
+++ CoHab/HomeViewController.swift
@@ -19,7 +19,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let items = ["Home", "Tasks", "Myschedule", "Bills", "Group", "Building"]
+        let items = ["Home", "Tasks", "Calendar", "Bills", "Group", "Building"]
        
         self.navigationController?.navigationBar.translucent = false
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
