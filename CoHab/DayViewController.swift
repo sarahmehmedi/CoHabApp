4535c15db74df579360ae29e989906b932f4addf
Nieky Allen <dallen4@luc.edu>
2016-04-26 14:28:53 -0500

    Started converting taskView and billView functionality to work with events. Will consult @clanzer and @smehmedi for help to make sure it actually works correctly.

--- CoHab/DayViewController.swift
+++ CoHab/DayViewController.swift
@@ -15,5 +15,118 @@ class DayViewController: UIViewController, /*UITableViewDataSource,*/ UITableVie
     let ref = Firebase(url: "https://cohabapp.firebaseio.com/events")
     var items = [NSDictionary]()
     
+    // These three variables are being pulled from the form to add a new label
+    var eventTitleString:String!
+    var eventDescString:String!
+    var eventStartString:String!
+    var eventEndString:String!
+    var eventLocationString:String!
+    var eventCreatorString:String!
     
+    // This is our table for events
+    @IBOutlet weak var table: UITableView!
+    
+    func loadDataFromFirebase(){
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        ref.observeEventType(.Value, withBlock: {snapshot in
+            var tempItems = [NSDictionary]()
+            
+            for item in snapshot.children{
+                let child = item as! FDataSnapshot
+                let dict = child.value as! NSDictionary
+                tempItems.append(dict)
+            }
+            
+            self.items = tempItems
+            print(tempItems)
+            self.table.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        })
+        self.table.reloadData()
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+        items = [NSDictionary]()
+        
+        loadDataFromFirebase()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Table view data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // This is how many rows are in the table, I just used the size of the "events" array
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    //Create table edited so theres a slide right function
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        
+        let reuseIdentifier = "programmaticCell"
+        //let dict = items[indexPath.row]
+        var cell = self.table.dequeueReusableCellWithIdentifier(reuseIdentifier) as! MGSwipeTableCell!
+        if cell == nil
+        {
+            cell = MGSwipeTableCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+        }
+        configureCell(cell, indexPath: indexPath)
+        
+        //configures right buttons:
+        
+        cell.rightButtons = [MGSwipeButton(title: "Delete", backgroundColor: UIColor.redColor(),callback: {
+            (sender: MGSwipeTableCell!) -> Bool in
+            print("Convenience callback for swipe buttons!")
+            
+            if self.items.count >= 1 {
+                tableView.beginUpdates()
+                let dict = self.items[indexPath.row]
+                let name = dict["eventTitle"] as! String
+                
+                let profile = self.ref.childByAppendingPath(name)
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                self.items.removeAtIndex(indexPath.row)
+                profile.removeValue()
+                
+                if self.items.count == 0 {
+                    tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                }
+                tableView.endUpdates()
+            }
+            
+            return true
+        })
+            ,MGSwipeButton(title: "More",backgroundColor: UIColor.lightGrayColor())]
+        cell.rightSwipeSettings.transition = MGSwipeTransition.Rotate3D
+        
+        
+        return cell
+    }
+    
+    
+    func configureCell(cell: MGSwipeTableCell, indexPath: NSIndexPath){
+        let dict = items[indexPath.row]
+        
+        let eTitle = dict["eventTitle"] as? String
+        let eDesc = dict["eventDesc"] as? String
+        let eStart = dict["eventStart"] as? String
+        let eEnd = dict["eventEnd"] as? String
+        let eLocation = dict["eventLocation"] as? String
+        let eCreator = dict["eventCreator"] as? String
+        
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        cell.textLabel?.text = eTitle
+        
+    }
 }
